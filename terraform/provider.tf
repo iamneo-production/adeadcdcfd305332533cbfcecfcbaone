552fd08c05abdfe83d58d40f559f2ce69a967876
Iamneo <techadmin@iamneo.ai>
2023-09-22 16:37:34 +0000

    Backup Commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,8 +11,9 @@ provider "aws" {
     access_key  = "AKIAYHH63OKC5AVS3IDL"
     secret_key  = "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"
 }
-resource {
-
+resource "aws_instance" "ubuntu_server"{
+    ami = "ami-0ed99df77a82560e6"
+    instance_type   = "t2.micro"
 }
 
 output "public_ip"{
