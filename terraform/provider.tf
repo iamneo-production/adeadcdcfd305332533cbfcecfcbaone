d73bee61eaa72c90ce18fb8b8df7103ad5cf9561
Iamneo <techadmin@iamneo.ai>
2023-09-22 16:32:33 +0000

    Backup Commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,5 +7,14 @@ terraform {
 }
 
 provider "aws" {
-    region    = 
+    region      = "ap-northeast-1" 
+    access_key  = "AKIAYHH63OKC5AVS3IDL"
+    secret_key  = "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"
+}
+resource {
+
+}
+
+output "public_ip"{
+    value   = aws_instance.demo-instance.public_ip
 }
